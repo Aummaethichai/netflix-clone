6905693263d7dae149d8be525b4849ea934ff9b6
Maytichai <maytichai2367@gmail.com>
2025-07-14 23:48:43 +0700

    บันทันความคืบหน้า

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import Image from "next/image";
-
+import TextField from '@mui/material/TextField';
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="relative flex flex-col bg-[url('/img/netfilx-login-wallpaper.jpg')] bg-top grow">
-        <div className="absolute inset-0 bg-black/50 z-0" />
+      <div className="relative flex flex-col bg-black sm:bg-[url('/img/netfilx-login-wallpaper.jpg')] bg-top grow">
+        <div className="absolute inset-0 bg-black/50 z-0"/>
         <div className="relative z-10">
-          <div className="mx-0 sm:mx-28">
+          <div className="flex sm:mx-5 w-[120px] md:mx-28 sm:w-[150px] justify-center">
             <Image
               src="/img/logo.png"
               alt="netflix-logo"
@@ -16,11 +16,13 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex justify-center items-center mb-15 z-10">
+        <div className="flex justify-center items-center sm:mb-15 z-10">
           <div className="bg-black/80 w-[30rem] h-[75vh] rounded-sm"> {/* bg-black bg-opacity-80 ใช้ไม่ได้ใน next 13+ เลยต้องใช้ bg-black/80 */}
-            <div className="flex flex-col container-from text-white w-full h-full p-10">
+            <div className="flex flex-col container-from text-white w-full h-full sm:p-10">
               <div className="text-[2rem] font-bold">เข้าสู่ระบบ</div>
-              <div>1</div>
+              <div className="">
+                 <TextField id="filled-basic" label="Filled" variant="filled" />
+              </div>
               <div>1</div>
               <div>1</div>
             </div>

abd04daf978fd2a511de6dbfc306529008229d04
Maytichai <maytichai2367@gmail.com>
2025-07-14 20:30:34 +0700

    แก้ไข wallpaper ให้สีดรอปลง

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col bg-[url('/img/netfilx-login-wallpaper.jpg')] bg-top grow">
-        <div className="">
+      <div className="relative flex flex-col bg-[url('/img/netfilx-login-wallpaper.jpg')] bg-top grow">
+        <div className="absolute inset-0 bg-black/50 z-0" />
+        <div className="relative z-10">
           <div className="mx-0 sm:mx-28">
             <Image
               src="/img/logo.png"
@@ -15,7 +16,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex justify-center items-center mb-15">
+        <div className="flex justify-center items-center mb-15 z-10">
           <div className="bg-black/80 w-[30rem] h-[75vh] rounded-sm"> {/* bg-black bg-opacity-80 ใช้ไม่ได้ใน next 13+ เลยต้องใช้ bg-black/80 */}
             <div className="flex flex-col container-from text-white w-full h-full p-10">
               <div className="text-[2rem] font-bold">เข้าสู่ระบบ</div>
@@ -26,8 +27,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex justify-items-end h-[45vh] bg-[#161616]">
-          Footer content here
+      <div className="flex justify-items-end h-[40vh] bg-[#161616] text-white">
+        Footer content here
       </div>
     </div>
   );
